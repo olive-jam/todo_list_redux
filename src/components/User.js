ad3f753571c15d5bd46c2f2250a267f2464fd7af
olive-jam <120158932+olive-jam@users.noreply.github.com>
2022-12-28 18:55:13 +0900

    todo_list

---
+++ src/components/User.js
@@ -0,0 +1,30 @@
+function User(props) {
+  return (
+    <div className="square-style">
+      <div className="todo-style">
+        <h2>{props.user.title}</h2>
+        <div>{props.user.body}</div>
+      </div>
+      <div className="twobtn-style">
+        <button
+          className="deleteBtn"
+          onClick={() => {
+            props.handleDelete(props.user.id);
+          }}
+        >
+          삭제
+        </button>
+        <button
+          className="doneBtn"
+          onClick={() => {
+            props.handleDone(props.user.id);
+          }}
+        >
+          {props.user.done ? "취소" : "완료"}
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default User;
